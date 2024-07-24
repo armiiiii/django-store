96b76d19a247cfe3ed71c0ae5ea836bb398e49f4
armi <superigorek74@gmail.com>
2024-07-24 20:32:46 +0500

    Authentication added

--- store/store/urls.py
+++ store/store/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 from rest_framework.routers import SimpleRouter
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from goods.views import ProductViewSet
 
 router = SimpleRouter()
@@ -10,6 +12,8 @@ router.register(r'products', ProductViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += router.urls
